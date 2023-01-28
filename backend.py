25776226e9e71c8eb29f5c80a8901da915325f1a
rtran1558 <rtran1558@yahoo.com>
2023-01-28 00:46:44 -0600

    comments to csv

--- backend.py
+++ backend.py
@@ -15,53 +15,54 @@ reddit = praw.Reddit(
     user_agent=os.environ["reddit_user_agent"],
 )
 
-def main():
-    # Create a regular expression pattern to match URLs
-    url_pattern = re.compile(r'https?://[^\s]+')
+# Create a regular expression pattern to match URLs
+url_pattern = re.compile(r'https?://[^\s]+')
 
+
+def main():
     print(reddit.read_only)
 
-    # Get input from user
+    # Get subreddit from user input
     subreddit = input("Enter the subreddit name: ")
 
-    # example for how to get the top 10 submissions in a subreddit inputed by the user
-    for submission in reddit.subreddit(subreddit).hot(limit=1):
-        print(submission)
+    # Get number of top posts to look at from user input
+    num_posts = int(input("Enter the number of subreddit posts to look at: "))
 
-        # Get the comments from each post
-        submission.comments.replace_more(limit=None)
+    # Get number of comments from a post to look at from user input
+    # num_comments = int(input("Enter the number of comments to look at: "))
+
+    grab_posts(subreddit,num_posts)
+
+
+def grab_posts(subreddit,num_posts):
+    # grab all the posts in a subreddit
+    for submission in reddit.subreddit(subreddit).hot(limit=num_posts):
+        # print(submission)
+
+        # reset variables
+        submission = ""
+        upvotes = 0
+        downvotes = 0
+
+        # Use the sub method to remove all URLs from the submission text
+        stripped_submission = url_pattern.sub('', submission.selftext)
+        print('this is the submission text: ',stripped_submission) 
+
+        # # total upvotes for submission
+        # upvotes = submission.ups
+        # print(upvotes)
+
+        # # total downvotes for submission
+        # downvotes = submission.downs
+        # print(downvotes)
 
         # loop through each comment and grab the text, upvotes, and downvotes
-        for comment in submission.comments.list()[:10]:
-            # reset variables
-            stripped_comment = ""
-            upvotes = 0
-            downvotes = 0
-
+        for comment in submission.comments.top(limit = 1):
             # Use the sub method to remove all URLs from the comment body
             stripped_comment = url_pattern.sub('', comment.body)
-            print(stripped_comment) 
-
-            # total upvotes for comment
-            upvotes = comment.ups
-            # print(upvotes)
-
-            # total downvotes for comment
-            downvotes = comment.downs
-            # print(downvotes)
-
-            classifier = pipeline("sentiment-analysis", return_all_scores= True)
-            res = classifier(stripped_comment)
-            print(res)
-            print("")
-
-        # # loop through each comment and grab the text, upvotes, and downvotes
-        # for comment in submission.comments.top(limit = 1):
-        #     # Use the sub method to remove all URLs from the comment body
-        #     stripped_comment = url_pattern.sub('', comment.body)
-        #     print(stripped_comment)
-        #     print(comment.ups)
-        #     print(comment.downs)
+            print(stripped_comment)
+            print(comment.ups)
+            print(comment.downs)
 
 
         
