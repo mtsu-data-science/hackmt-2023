d9a1e54d65d7aa6eac19bc5ab8bc956161dacf7f
Siri Alay <siri.alay1@gmail.com>
2023-01-28 16:26:02 -0600

    tweaked the title

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ df = read_subreddit_data_from_s3("test", "test-file-name")
 st.dataframe(df)
 
 # df = pd.read_csv("reddit_sentiment_analysis.csv")
-subreddit_title = st.text_input('Subreddit title', 'Enter a title!')
+subreddit_title = st.text_input('Subreddit title', 'Enter a subreddit title!')
 st.write("Subreddit title is ", subreddit_title)
 
-st.write(df.loc[df['Title'] == subreddit_title])
+st.write(df.loc[df['Submission'] == subreddit_title])
