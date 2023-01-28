5482549fe6bc785132bdbe44e4c41932e8332bb0
Alexander Antonison <adantonison@gmail.com>
2023-01-28 13:43:56 -0600

    update docs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 import streamlit as st
-from src.data_pipeline import example_func, read_subreddit_data_from_s3
+from src.data_pipeline import read_subreddit_data_from_s3
 
 st.title("Reddit Sentiment Analysis")
-
-example_func()
 
 df = read_subreddit_data_from_s3("test", "test-file-name")
 
