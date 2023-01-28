5a58ba785860c25e9740bb5dafa3b05a7736511b
Siri Alay <siri.alay1@gmail.com>
2023-01-28 16:55:44 -0600

    added comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,6 @@ df = read_subreddit_data_from_s3("test", "test-file-name")
 
 st.dataframe(df)
 
-# df = pd.read_csv("reddit_sentiment_analysis.csv")
 subreddit_title = st.text_input('Subreddit title', 'Enter a subreddit title!')
 st.write("Subreddit title is ", subreddit_title)
 
