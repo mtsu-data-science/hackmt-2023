cc2a55fbd18f3e06acb7dbae1d93bc219aa7de97
alex-antonison-mtsu <94069032+alex-antonison-mtsu@users.noreply.github.com>
2023-01-28 11:26:41 -0600

    Dev/data store idea (#7)
    * Add code for creating s3 bucket
    
    * Update requirements
    
    * Update notebook
    
    * Add s3 read from streamlit
    
    * Update readme
    
    ---------
    
    Co-authored-by: Alexander Antonison <adantonison@gmail.com>

--- src/data_pipeline.py
+++ src/data_pipeline.py
@@ -1,5 +1,20 @@
 import streamlit as st
+import awswrangler as wr
 
+s3_bucket_name = "data-science-club-hackmt-2023"
 
 def example_func():
     st.write("Hello World!")
+
+def write_subreddit_data_to_s3(df, subreddit_name, file_name):
+    wr.s3.to_parquet(
+        df=df,
+        path=f"s3://{s3_bucket_name}/subreddit_data/{subreddit_name}/{file_name}.parquet",
+        dataset=False,
+    )
+
+def read_subreddit_data_from_s3(subreddit_name, file_name):
+    return wr.s3.read_parquet(
+        path=f"s3://{s3_bucket_name}/subreddit_data/{subreddit_name}/{file_name}.parquet",
+        dataset=False,
+    )
