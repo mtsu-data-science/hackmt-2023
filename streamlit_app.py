7e7dbc15a2ab951c94e0be99c446de126e651849
Symone Randle <81542002+symonerandle@users.noreply.github.com>
2023-01-28 19:40:29 -0600

    onclick open results page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit_results
 from src.data_pipeline import read_subreddit_data_from_s3
 
 st.title("Reddit Sentiment Analysis")
@@ -8,4 +9,6 @@ df = read_subreddit_data_from_s3("test", "test-file-name")
 
 st.dataframe(df)
 
-st.button("Enter")
+results = {"streamlit_results": streamlit_results}
+
+st.button("Enter", onclick=results)
---
+++ streamlit_results.py
@@ -0,0 +1,4 @@
+import streamlit as st
+from src.data_pipeline import read_subreddit_data_from_s3
+
+st.title("Results")
