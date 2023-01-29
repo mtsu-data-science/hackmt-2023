72118d86f31d3ca192e50377a7e257335e955441
Symone Randle <81542002+symonerandle@users.noreply.github.com>
2023-01-28 23:15:05 -0600

    use with for subreddit_title

--- pages/streamlit_results.py
+++ pages/streamlit_results.py
@@ -3,3 +3,6 @@ from src.data_pipeline import read_subreddit_data_from_s3
 
 st.title("Results")
 #st.button("Reset")
+
+with subreddit_title:
+    st.write("Subreddit title is ", subreddit_title)
--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,8 @@ st.markdown("""---""")
 df = read_subreddit_data_from_s3("test", "test-file-name")
 
 subreddit_title = st.selectbox("Enter a subreddit title to analyze:", df)
-st.write("Subreddit title is ", subreddit_title)
+with subreddit_title:
+    st.write("Subreddit title is ", subreddit_title)
 #st.dataframe(df)
 
 #if st.button("< Prev"):
