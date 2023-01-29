92e511147ccb9387a4304fa784d2a83785666f26
Symone Randle <81542002+symonerandle@users.noreply.github.com>
2023-01-28 21:17:58 -0600

    update side bar

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,8 +33,8 @@ def nav_page(page_name, timeout_secs=3):
     """ % (page_name, timeout_secs)
     html(nav_script)
 
-st.button("About Us")
-st.sidebar.success("Results")
+#st.button("About Us")
+st.sidebar()
 st.markdown("""---""")
 df = read_subreddit_data_from_s3("test", "test-file-name")
 
