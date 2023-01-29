5ed0a6424b96f88aeafa42ff40c633fd52b823b1
Symone Randle <81542002+symonerandle@users.noreply.github.com>
2023-01-28 19:11:17 -0600

    add about us button

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from src.data_pipeline import read_subreddit_data_from_s3
 
-st.title("Reddit Sentiment Analysis")
+st.title("Reddit Sentiment Analysis").button("About Us")
 
 df = read_subreddit_data_from_s3("test", "test-file-name")
 
