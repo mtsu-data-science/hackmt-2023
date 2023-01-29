e987d52fbaa0100a707777c522180a8764414f1b
Symone Randle <81542002+symonerandle@users.noreply.github.com>
2023-01-28 23:00:03 -0600

    change from text_input to selectbox

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from src.data_pipeline import read_subreddit_data_from_s3
 
 st.title("Reddit Sentiment Analysis")
 
-subreddit_title = st.text_input('Enter a subreddit title to analyze:', '')
+subreddit_title = st.selectbox('Enter a subreddit title to analyze:', '')
 st.write("Subreddit title is ", subreddit_title)
 
 def nav_page(page_name, timeout_secs=3):
