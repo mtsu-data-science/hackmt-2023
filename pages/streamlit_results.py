8836b8f04d97e60f0b00c8ffa9375ded5710240f
Symone Randle <81542002+symonerandle@users.noreply.github.com>
2023-01-29 06:37:12 -0600

    added expander

--- pages/streamlit_results.py
+++ pages/streamlit_results.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 from src.data_pipeline import read_subreddit_data_from_s3
 
 #Page title
@@ -7,6 +9,14 @@ st.markdown("""---""")
 
 #Store session state into selection variable
 selection = st.write(st.session_state.rselection)
+
+df = pd.DataFrame(
+   np.random.randn(50, 10),
+   columns=('col %d' % i for i in range(10)))
+
+#st.dataframe(df)  # Same as st.write(df)
+with st.expander("Scores"):
+    st.dataframe(df)
 
 #Display session variable value if available
 if selection:
