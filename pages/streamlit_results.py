146802150b3bfaa30830145be033f3cc4815e4a2
Symone Randle <81542002+symonerandle@users.noreply.github.com>
2023-01-29 00:19:40 -0600

    clear results page

--- pages/streamlit_results.py
+++ pages/streamlit_results.py
@@ -5,8 +5,8 @@ from src.data_pipeline import read_subreddit_data_from_s3
 st.title("Results")
 #st.button("Reset")
 
-df = read_subreddit_data_from_s3("test", "test-file-name")
+#df = read_subreddit_data_from_s3("test", "test-file-name")
 
-subreddit_title = st.selectbox("Enter a subreddit title to analyze:", df)
-with subreddit_title:
-    st.write("Subreddit title is ", subreddit_title)
+#subreddit_title = st.selectbox("Enter a subreddit title to analyze:", df)
+#with subreddit_title:
+ #   st.write("Subreddit title is ", subreddit_title)
