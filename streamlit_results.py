9bf65e20a347e5450a28ed0890f4a464bc2909ba
Symone Randle <81542002+symonerandle@users.noreply.github.com>
2023-01-28 20:29:44 -0600

    removing st.set_page_config

--- streamlit_results.py
+++ streamlit_results.py
@@ -2,5 +2,5 @@ import streamlit as st
 import streamlit_app
 from src.data_pipeline import read_subreddit_data_from_s3
 
-st.set_page_config(page_title="Results")
+st.title("Results")
 st.button("Reset")
