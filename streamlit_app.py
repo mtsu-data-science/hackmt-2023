c2ae9029bf49b51c7bc791fb96984c73bf3ccf43
Symone Randle <81542002+symonerandle@users.noreply.github.com>
2023-01-29 00:43:57 -0600

    create get_selection function

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,10 +37,13 @@ st.sidebar.success("Results")
 st.markdown("""---""")
 df = read_subreddit_data_from_s3("test", "test-file-name")
 
-subreddit_title = st.selectbox("Enter a subreddit title to analyze:", df)
+def get_selection():
+    subreddit_title = st.selectbox("Enter a subreddit title to analyze:", df)
+    with subreddit_title:
+        st.write("Subreddit title is ", subreddit_title)
+    return subreddit_title
 
-with subreddit_title:
-    st.write("Subreddit title is ", subreddit_title)
+get_selection()
 
 #subreddit_title = st.selectbox("Enter a subreddit title to analyze:", df)
 #with subreddit_title:
