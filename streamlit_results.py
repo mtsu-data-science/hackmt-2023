9b58ef5b215c3f2b3aa234a2409bfe97e745913c
Symone Randle <81542002+symonerandle@users.noreply.github.com>
2023-01-28 20:20:59 -0600

    testing multi page app

--- streamlit_results.py
+++ streamlit_results.py
@@ -2,4 +2,5 @@ import streamlit as st
 import streamlit_app
 from src.data_pipeline import read_subreddit_data_from_s3
 
-st.title("Results")
+st.set_page_config(page_title="Results")
+st.sidebar("Reset")
